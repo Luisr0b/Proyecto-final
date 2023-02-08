e9bc33939d0981235b73bdcf5b13caf48287f6e4
Luisr0b <luis.robles7678@alumnos.udg.mx>
2023-02-08 16:54:13 -0600

    Se agregaron las canciones y se muestra una por una

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/canciones/{id?}', function ($id = null) {
+    $canciones = [];
+    $canciones[] = ['nombre' => 'Vive La Vida', 'artista' => 'Coldplay'];
+    $canciones[] = ['nombre' => 'Enseñame a bailar', 'artista' => 'Bad Bunny'];
+    $canciones[] = ['nombre' => 'Atlantis', 'artista' => 'Seafret'];
+
+    if(!is_null($id)) {
+        $cancion = $canciones[$id];
+    } else {
+        $cancion = null;
+    }
+    //return view('canciones')->with(['canciones' => $canciones]);
+    return view('canciones', compact('canciones', 'cancion'));
+});
+
+Route::get('/canciones/{id}', function ($id) {
+    $canciones = [];
+    $canciones[] = ['nombre' => 'Vive La Vida', 'artista' => 'Coldplay'];
+    $canciones[] = ['nombre' => 'Enseñame a bailar', 'artista' => 'Bad Bunny'];
+    $canciones[] = ['nombre' => 'Atlantis', 'artista' => 'Seafret'];
+
+    $cancion = $canciones[$id];
+
+    
+    return view('detallecancion', compact('cancion'));
+});
