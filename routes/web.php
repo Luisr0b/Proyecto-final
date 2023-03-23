bfe111f997622ff8f8d1dcd105fd1bda664f2d0f
Luis Uriel Robles Estrada <89150007+Luisr0b@users.noreply.github.com>
2023-03-23 10:28:27 -0600

    Eliminación
    Se eliminaron archivos

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,6 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/canciones/{id?}', [PaginasController::class, 'canciones']);
-Route::get('/contacto', [PaginasController::class, 'contacto']);
-Route::post('/contacto', [PaginasController::class, 'postContacto']);
 
 Route::resource('/empleado', EmpleadoController::class);
 Route::middleware([
