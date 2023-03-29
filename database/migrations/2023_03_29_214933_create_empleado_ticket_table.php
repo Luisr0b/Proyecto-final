3dc500a9f49c850a7c5d3c02edbf6bd20bec7a37
Luisr0b <luis.robles7678@alumnos.udg.mx>
2023-03-29 16:41:13 -0600

    Se modifico el color

--- database/migrations/2023_03_29_214933_create_empleado_ticket_table.php
+++ database/migrations/2023_03_29_214933_create_empleado_ticket_table.php
@@ -13,11 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('tickets', function (Blueprint $table) {
+        Schema::create('empleado_ticket', function (Blueprint $table) {
             $table->id();
-            $table->string('problema');
-            $table->string('solucion');
-            $table->string('atendio');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tickets');
+        Schema::dropIfExists('empleado_ticket');
     }
 };
